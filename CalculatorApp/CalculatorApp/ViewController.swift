0ecdcbba20ae9e32b65b952bd07d0c7118d5bcad
Crois0509 <crois0509@naver.com>
2024-11-15 10:41:47 +0900

    [Feat] VStack 세팅 메소드 추가

--- CalculatorApp/CalculatorApp/ViewController.swift
+++ CalculatorApp/CalculatorApp/ViewController.swift
@@ -45,17 +45,30 @@ class ViewController: UIViewController {
         ])
     }
     
-    /// 스택뷰의 기본 세팅을 해주는 메소드
-    /// - Parameters:
-    ///   - stackView: 세팅을 진행할 스택뷰
-    ///   - item: 스택뷰에 넣을 아이템
-    ///   - superView: 스택뷰의 오토레이아웃의 기준이 될 뷰
-    ///   - constant: 기준 뷰와 떨어질 값
-    ///
-    ///   ``setButtonRow(_:)``
     private func setHStack(_ stackViews: [UIStackView]) {
         for (index, stackView) in stackViews.enumerated() {
             
         }
+    }
+    
+    private func setVStack() {
+        setHStack([firstRowStack,
+                   secondRowStack,
+                   thirdRowStack,
+                   fourthRowStack])
+        
+        numberButtonsStack.addArrangedSubviews([firstRowStack,
+                                                secondRowStack,
+                                                thirdRowStack,
+                                                fourthRowStack])
+        
+        numberButtonsStack.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(numberButtonsStack)
+        
+        NSLayoutConstraint.activate([
+            numberButtonsStack.widthAnchor.constraint(equalToConstant: 350),
+            numberButtonsStack.topAnchor.constraint(equalTo: displayLabel.bottomAnchor, constant: 60),
+            numberButtonsStack.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
     }
 }
