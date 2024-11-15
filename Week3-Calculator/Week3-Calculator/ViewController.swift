4cc81861b7ce671ddccb1c3828e655569da0e697
chhue <chhue96@gmail.com>
2024-11-15 17:09:38 +0900

    [Feat] 숫자 표기 및 오토레이아웃 설정 (NSLayoutConstraint 이용)

--- Week3-Calculator/Week3-Calculator/ViewController.swift
+++ Week3-Calculator/Week3-Calculator/ViewController.swift
@@ -8,10 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let valueLabel = UILabel()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .black
+        
+        configureLabel()
+    }
+    
+    private func configureLabel() {
+        
+        valueLabel.text = "12345"
+        valueLabel.textColor = .white
+        valueLabel.textAlignment = .right
+        valueLabel.font = .systemFont(ofSize: 60)
+        valueLabel.translatesAutoresizingMaskIntoConstraints = false
+//        valueLabel.backgroundColor = .white
+        view.addSubview(valueLabel)
+        
+        NSLayoutConstraint.activate([
+            valueLabel.heightAnchor.constraint(equalToConstant: 100),
+            valueLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
+            valueLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            valueLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30)
+        ])
+        
     }
 
 
