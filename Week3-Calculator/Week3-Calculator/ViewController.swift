f6a702a3487bc2bbee0f2c526cf5d1f9c07bd79b
chhue <chhue96@gmail.com>
2024-11-15 16:05:55 +0900

    [Feat] 숫자 표기 및 오토레이아웃 설정

--- Week3-Calculator/Week3-Calculator/ViewController.swift
+++ Week3-Calculator/Week3-Calculator/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let valueLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureUI()
     }
-
+    
+    private func configureUI() {
+    
+        view.backgroundColor = .black
+        
+        valueLabel.text = "12345"
+        valueLabel.textColor = .white
+        valueLabel.textAlignment = .right
+        valueLabel.font = .systemFont(ofSize: 60)
+        valueLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(valueLabel)
+        
+        valueLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 200).isActive = true
+        valueLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
+        valueLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
+        valueLabel.heightAnchor.constraint(equalToConstant: 100).isActive = true
+    }
 
 }
 
