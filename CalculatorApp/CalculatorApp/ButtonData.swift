04b33525ec1ec46bdc03075afcfbeb79d37ca2c7
Crois0509 <crois0509@naver.com>
2024-11-15 10:33:51 +0900

    [Refactor] setButtonRow 메소드 수정

--- CalculatorApp/CalculatorApp/ButtonData.swift
+++ CalculatorApp/CalculatorApp/ButtonData.swift
@@ -23,26 +23,55 @@ class ButtonData {
     /// 파라미터 변수를 UIButton 배열로 반환해주는 함수
     /// - Parameter rows: UIButton으로 변환할 데이터
     /// - Returns: 변환된 UIButton을 담은 배열 -> [UIButton]
-    func setButtonRow(_ rows: [String]) -> [UIButton] {
+    func setButtonRow(_ rows: [[String]]) -> [[UIButton]] {
         var buttons = [UIButton]()
+        var buttonsList = [[UIButton]]()
         
         for row in rows {
-            let button = UIButton()
+            for data in row {
+                let button = UIButton()
+                var config = UIButton.Configuration.filled()
+                
+                var titleAttr = AttributedString.init(data)
+                titleAttr.font = .systemFont(ofSize: 30, weight: .bold)
+                
+                config.attributedTitle = titleAttr
+                config.baseBackgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
+                config.baseForegroundColor = .white
+                config.cornerStyle = .capsule
+                
+                button.configuration = config
+                button.translatesAutoresizingMaskIntoConstraints = false
+                
+                button.configurationUpdateHandler = { 
+                    switch $0.state {
+                    case .normal:
+                        $0.configuration?.baseBackgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
+                        $0.configuration?.baseForegroundColor = .white
+                        
+                    case .highlighted:
+                        $0.configuration?.baseBackgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0).withAlphaComponent(0.7)
+                        $0.configuration?.baseForegroundColor = .white.withAlphaComponent(0.7)
+                        
+                    default:
+                        break
+                    }
+                }
+                
+                NSLayoutConstraint.activate([
+                    button.heightAnchor.constraint(equalToConstant: 80),
+                    button.widthAnchor.constraint(equalToConstant: 80)
+                ])
+                
+                buttons.append(button)
+            }
             
-            button.setTitle(row, for: .normal)
-            button.titleLabel?.font = .boldSystemFont(ofSize: 30)
-            button.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
-            button.layer.cornerRadius = 40
-            button.translatesAutoresizingMaskIntoConstraints = false
+            buttonsList.append(buttons)
+            buttons.removeAll()
             
-            NSLayoutConstraint.activate([
-                button.heightAnchor.constraint(equalToConstant: 80),
-                button.widthAnchor.constraint(equalToConstant: 80)
-            ])
-            
-            buttons.append(button)
         }
         
-        return buttons
+        return buttonsList
     }
 }
+
