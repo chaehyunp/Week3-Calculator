21e558878c8e4b21b2ca6a8cce056a994860ad8c
ericKwon95 <ericyong95@gmail.com>
2024-11-18 23:42:20 +0900

    feat: level1 기능 구현

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -8,14 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private let numericDisplay: UILabel = {
+        let label = UILabel()
+        label.text = "12345"
+        label.textAlignment = .right
+        label.font = .systemFont(ofSize: 60, weight: .bold)
+        label.textColor = .white
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        self.view.backgroundColor = .red
+        configureUI()
     }
-
-
+    
+    func configureUI() {
+        view.backgroundColor = .black
+        
+        let subViews = [
+            numericDisplay
+        ]
+        
+        subViews.forEach {
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+            numericDisplay.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            numericDisplay.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            numericDisplay.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
+            numericDisplay.heightAnchor.constraint(equalToConstant: 100)
+        ])
+    }
 }
 
 #Preview {
