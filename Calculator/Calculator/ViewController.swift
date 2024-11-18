06d186875eaad67a0f0f20bae9949570191ee6f0
ericKwon95 <ericyong95@gmail.com>
2024-11-18 23:07:26 +0900

    feat: 코드베이스 UI를 위한 사전설정

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -12,8 +12,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red
     }
 
 
+}
+
+#Preview {
+    ViewController()
 }
 
